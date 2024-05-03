4aa0e967efa21c3c447d583a5e15fcb9792916c6
ChaoHong1944 <91182144+ChaoHong1944@users.noreply.github.com>
2024-05-03 00:52:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
+
+//imports http package
+const http = require('http')
+
+//creates the http server
+const server = http.createServer(app)
+
 const port = 3000
 
 app.use(express.static('public'))
 
+//routing
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 console.log('server did not loaded')
+  
