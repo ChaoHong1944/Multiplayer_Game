b7677de6432c206068491513680e0fd4dc6b99fb
ChaoHong1944 <91182144+ChaoHong1944@users.noreply.github.com>
2024-05-03 18:28:48 -0400

    test
    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+// socket.io setup
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
@@ -15,13 +16,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+const players = {
+  oijewfasdf12: {
+    x: 100,
+    y: 100,
+    color: 'yellow'
+  },
+  asdfaslkj: {
+    x: 200,
+    y: 200,
+    color: 'red'
+  }
+}
+
 io.on('connection', (socket) => {
-  console.log('a user connected')
-})
+  console.log('connected');
+});
+
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-console.log('server did not loaded')
+console.log('server did loaded')
   
