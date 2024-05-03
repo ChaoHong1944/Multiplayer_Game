3a5abaa93a6df57dec51d8920013b6258f3f1f88
ChaoHong1944 <91182144+ChaoHong1944@users.noreply.github.com>
2024-05-03 18:56:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
 const express = require('express')
 const app = express()
 
-// socket.io setup
 const http = require('http')
 const server = http.createServer(app)
-const { Server } = require('socket.io')
-const io = new Server(server)
+const {Server} = require('socket.io')
+const io = new Server(server, { pingInterval:2000, pingTimeout:5000})
 
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('multiplayer-game-starter/public'))
 
-//routing
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-const players = {
-  oijewfasdf12: {
-    x: 100,
-    y: 100,
-    color: 'yellow'
-  },
-  asdfaslkj: {
-    x: 200,
-    y: 200,
-    color: 'red'
-  }
-}
+const backendPlayers ={}
+const SPEED = 10
 
 io.on('connection', (socket) => {
-  console.log('connected');
-});
+  console.log('a user connected')
+  backendPlayers[socket.id] ={
+    x:500 * Math.random(),
+    y:500 * Math.random(),
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`,
+    sequenceNum: 0
+  }
+  io.emit('updatePlayers', backendPlayers)
 
+  socket.on('disconnect', (reason) =>{
+    console.log(reason)
+    delete backendPlayers[socket.id]
+    io.emit('updatePlayers', backendPlayers)
+  })
+  
+  socket.on('keydown', ({keycode, sequenceNum}) => {
+    backendPlayers[socket.id].sequenceNum = sequenceNum
+    switch(keycode){
+      case 'KeyW':
+        backendPlayers[socket.id].y -= SPEED
+        break
+      case 'KeyA':
+        backendPlayers[socket.id].x -= SPEED
+        break
+      case 'KeyS':
+        backendPlayers[socket.id].y += SPEED
+        break
+      case 'KeyD':
+        backendPlayers[socket.id].x += SPEED
+        break
+      }
+  })
+  console.log(backendPlayers)
+})
+
+setInterval(() => {
+  io.emit('updatePlayers', backendPlayers)
+}, 15)
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-console.log('server did loaded')
-  
+console.log('server loaded')
