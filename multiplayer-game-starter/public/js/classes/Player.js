036232a393df2d282f228c4d85f0f6072b98bdcf
ChaoHong1944 <91182144+ChaoHong1944@users.noreply.github.com>
2024-05-10 00:12:31 -0400

    Game polishes
    - improved front end latency
    - centered display to fix all user views

--- multiplayer-game-starter/public/js/classes/Player.js
+++ multiplayer-game-starter/public/js/classes/Player.js
@@ -1,15 +1,33 @@
-class Player {
-  constructor({x, y, radius, color}) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+
+class Player{
+  constructor({x, y, radius, color, username}) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.username = username;
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius * window.devicePixelRatio, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
+    c.font = '12px sans-serif';  // Ensure the font size and family is correctly specified
+    c.fillStyle = 'white';
+
+    // Measure the text width
+    const textWidth = c.measureText(this.username).width;
+
+    // Center the text below the player model
+    const textX = this.x - textWidth / 2;
+    const textY = this.y + 20;  // Adjust Y offset as needed to position the text below the player model
+
+    c.fillText(this.username, textX, textY);
+
+    c.save();
+    c.shadowColor = this.color;
+    c.shadowBlur = 20;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius * window.devicePixelRatio, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
   }
 }
